f9887a4e6e4c1127d9052c9b41c79d961640c9d6
sksayz5 <137992110+sksayz5@users.noreply.github.com>
2023-07-01 10:23:53 +0530

    Update sync_rom.sh

--- scripts/sync_rom.sh
+++ scripts/sync_rom.sh
@@ -17,16 +17,4 @@ git clone $DEVICE_REPO --depth=1 -b $DEVICE_BRANCH "device/$OEM/$CODENAME";
 git clone $VENDOR_REPO --depth=1 -b $VENDOR_BRANCH "vendor/$OEM/$CODENAME";
 git clone $KERNEL_REPO --depth=1 -b $KERNEL_BRANCH "kernel/$OEM/$CODENAME";
 
-#clone mtk hardware
-git clone --depth=1 https://github.com/ArrowOS/android_hardware_mediatek hardware/mediatek;
-
-#clone Sepolicy
-git clone --depth=1 https://github.com/ArrowOS/android_device_mediatek_sepolicy_vndr -b arrow-13.1 device/mediatek/sepolicy_vndr;
-
-#Clone Clang
-git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-r437112 prebuilts/clang/host/linux-x86/clang-r437112;
-
-#Clone Ims
-git clone --depth=1 https://github.com/ArrowOS-Devices/android_vendor_realme_RMX2020-ims vendor/realme/RMX2020-ims;
-
 exit 0;
